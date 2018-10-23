efcc26e3732de33e450682285f307081a747104a
Vibhushit Singhal <41487076+Vibhushit07@users.noreply.github.com>
2018-10-23 14:04:33 +0530

    Update bubbleSort.c

--- ALGO/C/Sort/bubbleSort.c
+++ ALGO/C/Sort/bubbleSort.c
@@ -3,8 +3,8 @@
 void bubbleSort(int *arr, int n)
 {
     int i, j, temp;
-    for (i = 0; i < n; i++)
-        for (j = 0; j < n - 1; j++)
+    for (i = 0; i < n; i++)                 // for (i = 0; i < n -1; i++)
+        for (j = 0; j < n - 1; j++)         //     for(j = 0; j < n - i - 1; j++)
             if (arr[j + 1] < arr[j])
             {
                 temp = arr[j];
